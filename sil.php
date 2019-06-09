3a625e3eea9ae7451d7df9483197ba9b5c799ce9
muhammedkerimkilicoz <49927149+muhammedkerimkilicoz@users.noreply.github.com>
2019-06-09 14:46:00 +0300

    Update sil.php

--- sil.php
+++ sil.php
@@ -3,12 +3,12 @@
 if ($_GET) 
 {
 
-include("ayar.php"); // veritabanı bağlantımızı sayfamıza ekliyoruz.
+include("ayar.php"); 
 
-// id'si seçilen veriyi silme sorgumuzu yazıyoruz.
+
 if (mysqli_query($conn,"DELETE FROM urunler WHERE id =".(int)$_GET['id'])) 
 {
-    header("location:siluyari.php"); // Eğer sorgu çalışırsa ekle.php sayfasına gönderiyoruz.
+    header("location:siluyari.php"); 
 }
 }
 
